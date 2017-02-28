da5318545ec64ae0cc14260696ebc8d471d5072a
late-autumn <late_autumn88@naver.com>
2017-02-28 10:43:10 +0900

    HTML5, css 마무리

---
+++ Web/src/com/test/LoginServlet.java
@@ -0,0 +1,54 @@
+package com.test;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/Login")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public LoginServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String userid = request.getParameter("userid");
+		String passwd = request.getParameter("password");
+		String name	= request.getParameter("name");
+		String mail = request.getParameter("email");
+		
+		response.setContentType("text/html;charset=UTF-8"); 
+		PrintWriter out = response.getWriter();
+		out.print("<html><body>");
+		out.print("아이디:"+userid+"<br>");
+		out.print("비밀번호:"+passwd+"<br>");
+		out.print("이름:"+name+"<br>");
+		out.print("메일:"+mail+"<br>");
+		out.print("</body></html>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
